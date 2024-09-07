a71b9e0be171c720da5e560969c99778e8edb0d3
AlexDyakonov <shram.monolit@mail.ru>
2024-09-07 18:44:43 +0300

    Some updates

--- app_main_page.js
+++ app_main_page.js
@@ -1,4 +1,29 @@
-let tg = window.Telegram.WebApp;
+// let tg = window.Telegram.WebApp;
 
-tg.expand()
-tg.ready();
+// tg.expand();
+// tg.ready();
+
+const chessboard = document.getElementById('chessboard');
+
+function createChessboard() {
+    for (let i = 0; i < 64; i++) {
+        const square = document.createElement('div');
+        
+
+        square.classList.add('square');
+        chessboard.appendChild(square);
+
+        if (Math.floor(i / 8) % 2 === 0) {
+            square.classList.add(i % 2 === 0 ? 'dark' : 'light');
+        } else {
+            square.classList.add(i % 2 === 0 ? 'light' : 'dark');
+        }
+        // console.log("Added square " + i);
+
+        square.addEventListener('click', function () {
+            console.log("Clicked square " + i)
+        });
+    }
+}
+
+createChessboard();
